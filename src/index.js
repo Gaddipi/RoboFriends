d49bb0ee85061e857d7d5d68e285e0d589bdf2f4
Ismail Gaddipati <Ismail@IMAC.fios-router.home>
2020-04-03 13:44:03 -0400

    redux added_ thunk, redux logger

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import { Provider } from "react-redux";
+import thunkMiddleware from "redux-thunk";
+import { createLogger } from "redux-logger";
 import App from "./containers/App";
 import * as serviceWorker from "./serviceWorker";
 import "tachyons";
+import { searchRobots, requestRobots } from "./reducers";
+
+const rootReducer = combineReducers({ searchRobots, requestRobots });
+const logger = createLogger();
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunkMiddleware, logger)
+);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
